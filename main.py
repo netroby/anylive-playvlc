f2138ce14002d1008a01f4132f75c1ff4ed697f9
ZhiFeng Hu <huzhifeng@douyu.tv>
2017-10-26 17:47:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,19 @@ class MyWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Any Live play via vlc")
         self.set_border_width(10)
+        self.set_default_size(480,48)
 
         self.box = Gtk.Box(spacing = 6)
         self.add(self.box)
 
         self.entry = Gtk.Entry()
         self.entry.connect("activate", self.on_button_clicked)
+        self.entry.set_size_request(320, 32)
         self.box.pack_start(self.entry, True, True, 0)
 
         self.button = Gtk.Button(label="Play")
         self.button.connect("clicked", self.on_button_clicked)
+        self.button.set_size_request(40, 32)
         self.box.pack_start(self.button, True, True, 0)
 
 
@@ -33,4 +36,5 @@ class MyWindow(Gtk.Window):
 win = MyWindow()
 win.connect("delete-event", Gtk.main_quit)
 win.show_all()
+
 Gtk.main()
